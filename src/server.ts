a786b82a80b834b97542ee50ebbb9381d0b3e910
Andrew Nelson <andy@andyhub.com>
2024-05-20 17:08:49 -0400

    improved initializer

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import errorHandler from './_middleware/error-handler.js';
 import { initialize } from './_helpers/db.js';
 import userRouter from './users/users.controller.js';
 import taskRouter from './tasks/tasks.controller.js';
-import { create as createUser } from './users/user.service.js';
+import { create as createUser, getAll } from './users/user.service.js';
 import { create as createTask } from './tasks/task.service.js';
 
 const APP: Application = express();
@@ -42,20 +42,22 @@ initialize()
     if (process.env.NODE_ENV === 'dev') {
       await createUser({ username: 'user', password: 'password' }).catch(
         console.error
-      );
+      )
+      const newUserId = (await getAll())[0].id
+      
       await createTask({
         id: 1,
         data: 'You can drag tasks around, mark them as done, delete, pin to the top of this list, and toggle between regular and checklist types',
         done: false,
         pinned: false,
-        user_id: 1,
+        user_id: newUserId,
       });
       await createTask({
         id: 2,
         data: 'This is a regular task',
         done: false,
         pinned: false,
-        user_id: 1,
+        user_id: newUserId,
       });
       await createTask({
         id: 3,
@@ -73,7 +75,7 @@ initialize()
         ],
         done: false,
         pinned: false,
-        user_id: 1,
+        user_id: newUserId,
       });
     }
   })
