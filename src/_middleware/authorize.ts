91c486dab97df21b0069c1ca34a65e38cc0b5910
Andrew Nelson <andrew.nelson@dowjones.com>
2025-12-28 13:51:01 -0500

    better error handling for auth

--- src/_middleware/authorize.ts
+++ src/_middleware/authorize.ts
@@ -2,8 +2,13 @@ import db from '../_helpers/db.js';
 
 export default function authorize() {
   return async (req, res, next) => {
+    const token = req.cookies?.token;
+    if (!token) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
     const user = await db.User.findOne({
-      where: { session: req.cookies?.token },
+      where: { session: token },
     });
     if (!user) return res.status(401).json({ message: 'Unauthorized' });
     req.user = user.get();
